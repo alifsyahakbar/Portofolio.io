a570a8fd2f111b31bfcfd0023b6a434bdf83f54c
alifsyahakbar <alifsyah789@gmail.com>
2022-07-06 01:47:49 +0700

    upload portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container : {
+      center : true,
+      padding : '16px'
+    },
+    extend: {
+      colors : {
+        primary : '#14b8a6',
+        dark : '#27272a'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
